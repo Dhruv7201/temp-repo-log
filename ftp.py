07d3ec499d4d915fca52c43adf0d9011d3fe6288
Dhruv Modi <91967043+Dhruv7201@users.noreply.github.com>
2024-05-14 13:33:31 +0530

    Create ftp.py

---
+++ ftp.py
@@ -0,0 +1,19 @@
+from ftplib import FTP
+from datetime import datetime
+
+def upload_file_to_ftp(hostname, username, password, local_file_path, remote_file_path):
+    ftp = FTP(hostname, username, password)
+
+    with open(local_file_path, 'rb') as file:
+        ftp.storbinary(f'STOR {remote_file_path}', file)
+    ftp.close()
+
+# Example usage:
+hostname = '164.52.196.173'
+username = 'ethicsdev'
+
+password = 'Ethics@2024$$'
+local_file_path = f'/var/prod/log/MCode_102VPoint_2{datetime.now().strftime("%Y-%m-%d")}_log.txt'
+remote_file_path = f'Mcode_102/Vpoint_2/{datetime.now().strftime("%Y-%m-%d")}_log.txt'
+
+upload_file_to_ftp(hostname, username, password, local_file_path, remote_file_path)
