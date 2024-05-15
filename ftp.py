b941db9d72ea34f86d8b27f91a458367b2ebf2f3
Dhruv Modi <91967043+Dhruv7201@users.noreply.github.com>
2024-05-15 10:19:21 +0530

    Update ftp.py

--- ftp.py
+++ ftp.py
@@ -14,6 +14,6 @@ username = 'tapovan'
 
 password = 'Tapovan@2024'
 local_file_path = f'/var/prod/log/{datetime.now().strftime("%Y-%m-%d")}_log.txt'
-remote_file_path = f'Mcode_102/VPoint_2/{datetime.now().strftime("%Y-%m-%d")}_log.txt'
+remote_file_path = f'VPoint_2/Mcode_102/{datetime.now().strftime("%Y-%m-%d")}_log.txt'
 
 upload_file_to_ftp(hostname, username, password, local_file_path, remote_file_path)
